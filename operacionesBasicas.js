dea8c1c51d352a1de55155f9315d254a7cc5239f
Andres_Marulanda <afmsamsung1981@gmail.com>
2023-11-13 12:54:06 -0500

    Ejercicios introducción a JS

---
+++ operacionesBasicas.js
@@ -0,0 +1,27 @@
+/** Programa sencillo de introducción a la sintaxis de JS */
+
+// Crear dos variables globales (var). JS no es un lenguaje de programación fuertemente tipado.
+var numero1 = 4
+var numero2 = 12
+
+// Realizar las 4 operaciones aritméticas básicas.
+var suma = numero1 + numero2
+var resta = numero2 - numero1
+var producto = numero1 * numero2
+var division = numero2 / numero1
+
+// Mostrar en pantalla los resultados.
+console.log("suma = " + suma)
+console.log("resta = " + resta)
+console.log("producto = " + producto)
+console.log("división = " + division)
+
+// Calcular el seno(3PI).
+console.log("sen(3PI) = " + Math.sin(3 * Math.PI))
+
+// Calcular la 3ra potencia de 6.
+console.log("6 ** 3 = " + Math.pow(6, 3))
+
+
+
+
